d0d63761e7a1dc546ac15f8205a5aacef238d696
cacious <caci0us@hotmail.com>
2023-07-27 20:26:43 +0800

    kunkun inf

---
+++ kunkun_inf.py
@@ -0,0 +1,19 @@
+# CACIOUS CODING
+# Data     : 7/27/23  5:05 PM
+# File name: kunkun_inf
+# Desc     :
+import torch
+from util.args_util import get_args
+from util.model_util import create_training_model
+from util.weight_util import load_model
+from util.infer_util import get_reid_normed
+
+args = get_args()
+args.device = torch.device("cpu")
+
+training_model = create_training_model(args)
+training_model = load_model(training_model, args.weight_path)
+model = training_model.model
+
+reid_normed = get_reid_normed(model, args.inf_img_path)
+print(reid_normed.detach().numpy().tolist())
